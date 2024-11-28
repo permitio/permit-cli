e447a0dd36264b0773ef4108089ef87e5160722b
Abinand P <abinand0911@gmail.com>
2024-11-28 13:26:07 +0530

    test:added index and PDPCheck
    Signed-off-by: Abinand P <abinand0911@gmail.com>

--- tests/components/AuthProvider.test.tsx
+++ tests/components/AuthProvider.test.tsx
@@ -12,7 +12,7 @@ vi.mock('../../source/lib/auth.js', () => ({
 
 describe('AuthProvider', () => {
 	it('should display loading text while loading token', async () => {
-		loadAuthToken.mockResolvedValueOnce(new Promise(() => {}));
+		(loadAuthToken as any).mockResolvedValueOnce(new Promise(() => {}));
 
 		const { lastFrame } = render(
 			<AuthProvider>
@@ -23,7 +23,9 @@ describe('AuthProvider', () => {
 		expect(lastFrame()).toContain('Loading Token');
 	});
 	it('should display error message if loading token fails', async () => {
-		loadAuthToken.mockRejectedValueOnce(new Error('Failed to load token'));
+		(loadAuthToken as any).mockRejectedValueOnce(
+			new Error('Failed to load token'),
+		);
 
 		const { lastFrame } = render(
 			<AuthProvider>
@@ -36,7 +38,7 @@ describe('AuthProvider', () => {
 	});
 
 	it('should display children when token is loaded successfully', async () => {
-		loadAuthToken.mockResolvedValueOnce('mocked-token');
+		(loadAuthToken as any).mockResolvedValueOnce('mocked-token');
 
 		const { lastFrame } = render(
 			<AuthProvider>
@@ -53,7 +55,7 @@ describe('AuthProvider', () => {
 			return <Text>{authToken || 'No token'}</Text>;
 		};
 
-		loadAuthToken.mockResolvedValueOnce('mocked-token');
+		(loadAuthToken as any).mockResolvedValueOnce('mocked-token');
 
 		const { lastFrame } = render(
 			<AuthProvider>
@@ -66,17 +68,19 @@ describe('AuthProvider', () => {
 	});
 
 	it('should throw an error when useAuth is called outside of AuthProvider', () => {
-		try {
-			const ChildComponent = () => {
+		const ChildComponent = () => {
+			let apiKey: string;
+			try {
 				const { authToken } = useAuth();
-				return <Text>{authToken || 'No token'}</Text>;
-			};
-			render(<ChildComponent />);
-		} catch (error: any) {
-			expect(error.message).toMatch(
-				/useAuth must be used within an AuthProvider/,
-			);
-			return;
-		}
+				apiKey = authToken;
+			} catch (error) {
+				return <Text>useAuth must be used within an AuthProvider</Text>;
+			}
+			return <Text>{apiKey || 'No token'}</Text>;
+		};
+		const { lastFrame } = render(<ChildComponent />);
+		expect(lastFrame()).toContain(
+			'useAuth must be used within an AuthProvider',
+		);
 	});
 });
