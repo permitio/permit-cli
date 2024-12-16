f06dfb98a8fa0b7f8c380aca523ff8d4bc0dbf98
35C4n0r <jaykumar20march@gmail.com>
2024-12-17 01:23:21 +0530

    feat: env commands use AuthProvider

--- source/commands/env/copy.tsx
+++ source/commands/env/copy.tsx
@@ -1,23 +1,20 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { Text } from 'ink';
+import React from 'react';
 import { option } from 'pastel';
-import { TextInput } from '@inkjs/ui';
 import zod from 'zod';
 import { type infer as zInfer } from 'zod';
-import { useApiKeyApi } from '../../hooks/useApiKeyApi.js';
-import { useEnvironmentApi } from '../../hooks/useEnvironmentApi.js';
-import EnvironmentSelection, {
-	ActiveState,
-} from '../../components/EnvironmentSelection.js';
-import { cleanKey } from '../../lib/env/copy/utils.js';
+import { AuthProvider } from '../../components/AuthProvider.js';
+import CopyComponent from '../../components/env/CopyComponent.js';
 
 export const options = zod.object({
-	key: zod.string().describe(
-		option({
-			description:
-				'API Key to be used for the environment copying (should be at least a project level key)',
-		}),
-	),
+	key: zod
+		.string()
+		.optional()
+		.describe(
+			option({
+				description:
+					'Optional: API Key to be used for the environment copying (should be at least a project level key). In case not set, CLI lets you select one',
+			}),
+		),
 	from: zod
 		.string()
 		.optional()
@@ -70,192 +67,20 @@ type Props = {
 	readonly options: zInfer<typeof options>;
 };
 
-interface EnvCopyBody {
-	existingEnvId?: string | null;
-	newEnvKey?: string | null;
-	newEnvName?: string | null;
-	newEnvDescription?: string | null;
-	conflictStrategy?: string | null;
-}
-
 export default function Copy({
-	options: {
-		key: apiKey,
-		from,
-		to: envToId,
-		name,
-		description,
-		conflictStrategy,
-	},
+	options: { key, from, to, name, description, conflictStrategy },
 }: Props) {
-	const [error, setError] = React.useState<string | null>(null);
-	const [authToken, setAuthToken] = React.useState<string | null>(null);
-	const [state, setState] = useState<
-		| 'loading'
-		| 'selecting-env'
-		| 'selecting-name'
-		| 'selecting-description'
-		| 'copying'
-		| 'done'
-	>('loading');
-	const [projectFrom, setProjectFrom] = useState<string | null>(null);
-	const [envToName, setEnvToName] = useState<string | undefined>(name);
-	const [envFrom, setEnvFrom] = useState<string | undefined>(from);
-	const [envToDescription, setEnvToDescription] = useState<string | undefined>(
-		description,
-	);
-
-	const { validateApiKeyScope } = useApiKeyApi();
-	const { copyEnvironment } = useEnvironmentApi();
-
-	useEffect(() => {
-		if (error || state === 'done') {
-			process.exit(1);
-		}
-	}, [error, state]);
-
-	useEffect(() => {
-		const handleEnvCopy = async (envCopyBody: EnvCopyBody) => {
-			let body = {};
-			if (envCopyBody.existingEnvId) {
-				body = {
-					target_env: { existing: envCopyBody.existingEnvId },
-				};
-			} else if (envCopyBody.newEnvKey && envCopyBody.newEnvName) {
-				body = {
-					target_env: {
-						new: {
-							key: cleanKey(envCopyBody.newEnvKey),
-							name: envCopyBody.newEnvName,
-							description: envCopyBody.newEnvDescription ?? '',
-						},
-					},
-				};
-			}
-			if (conflictStrategy) {
-				body = {
-					...body,
-					conflict_strategy: envCopyBody.conflictStrategy ?? 'fail',
-				};
-			}
-			const { error } = await copyEnvironment(
-				projectFrom ?? '',
-				envFrom ?? '',
-				apiKey,
-				null,
-				body,
-			);
-			if (error) {
-				setError(`Error while copying Environment: ${error}`);
-				return;
-			}
-			setState('done');
-		};
-
-		if (
-			((envToName && envToDescription && conflictStrategy) || envToId) &&
-			envFrom &&
-			projectFrom
-		) {
-			setState('copying');
-			handleEnvCopy({
-				newEnvKey: envToName,
-				newEnvName: envToName,
-				newEnvDescription: envToDescription,
-				existingEnvId: envToId,
-				conflictStrategy: conflictStrategy,
-			});
-		}
-	}, [
-		apiKey,
-		conflictStrategy,
-		copyEnvironment,
-		envFrom,
-		envToDescription,
-		envToId,
-		envToName,
-		projectFrom,
-	]);
-
-	useEffect(() => {
-		// Step 1, we use the API Key provided by the user &
-		// checks if the api_key scope >= project_level &
-		// sets the apiKey and sets the projectFrom
-
-		(async () => {
-			const { valid, scope, error } = await validateApiKeyScope(
-				apiKey,
-				'project',
-			);
-			if (!valid || error) {
-				setError(error);
-				return;
-			} else if (scope && valid) {
-				setProjectFrom(scope.project_id);
-				setAuthToken(apiKey);
-			}
-		})();
-	}, [apiKey, validateApiKeyScope]);
-
-	const handleEnvFromSelection = useCallback(
-		(
-			_organisation_id: ActiveState,
-			_project_id: ActiveState,
-			environment_id: ActiveState,
-		) => {
-			setEnvFrom(environment_id.value);
-		},
-		[],
-	);
-
-	useEffect(() => {
-		if (!envFrom) {
-			setState('selecting-env');
-		} else if (!envToName && !envToId) {
-			setState('selecting-name');
-		} else if (!envToDescription && !envToId) {
-			setState('selecting-description');
-		}
-	}, [envFrom, envToDescription, envToId, envToName]);
-
 	return (
 		<>
-			{state === 'selecting-env' && authToken && (
-				<>
-					<Text>Select an existing Environment to copy from.</Text>
-					<EnvironmentSelection
-						accessToken={authToken}
-						cookie={''}
-						onComplete={handleEnvFromSelection}
-						onError={setError}
-					/>
-				</>
-			)}
-			{authToken && state === 'selecting-name' && (
-				<>
-					<Text>Input the new Environment name to copy to.</Text>
-					<TextInput
-						onSubmit={name => {
-							setEnvToName(name);
-						}}
-						placeholder={'Enter name here...'}
-					/>
-				</>
-			)}
-			{authToken && state === 'selecting-description' && (
-				<>
-					<Text>Input the new Environment Description.</Text>
-					<TextInput
-						onSubmit={description => {
-							setEnvToDescription(description);
-						}}
-						placeholder={'Enter description here...'}
-					/>
-				</>
-			)}
-
-			{state === 'done' && <Text>Environment copied successfully</Text>}
-			{error && <Text>{error}</Text>}
+			<AuthProvider permit_key={key} scope={'project'}>
+				<CopyComponent
+					from={from}
+					to={to}
+					name={name}
+					description={description}
+					conflictStrategy={conflictStrategy}
+				/>
+			</AuthProvider>
 		</>
 	);
 }
