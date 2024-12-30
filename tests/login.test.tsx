7daf0b951d2008d6e50fb62e6a665da226c9f1d2
Abinand P <abinand0911@gmail.com>
2024-12-30 11:01:55 +0000

    test: fixed additional keytar depdency
    Signed-off-by: Abinand P <abinand0911@gmail.com>

--- tests/login.test.tsx
+++ tests/login.test.tsx
@@ -3,6 +3,14 @@ import { vi, expect, it, describe } from 'vitest';
 import { render } from 'ink-testing-library';
 import Login from '../source/commands/login';
 import delay from 'delay';
+import * as keytar from "keytar"
+
+vi.mock("keytar",()=>({
+	getPassword: vi.fn(),
+	setPassword: vi.fn(),
+	deletePassword:vi.fn()
+}))
+
 describe('Login Component', () => {
 	it('Should render the login component', async () => {
 		const { lastFrame } = render(
