5b1d950d9b03a5365e3f4ac8948008760a430d9e
35C4n0r <jaykumar20march@gmail.com>
2025-01-10 08:47:50 +0530

    chore: minor improv

--- source/lib/auth.ts
+++ source/lib/auth.ts
@@ -1,8 +1,7 @@
 import { createHash, randomBytes } from 'node:crypto';
 import * as http from 'node:http';
 import open from 'open';
-import keytar from 'keytar';
-const { setPassword, getPassword, deletePassword } = keytar;
+import * as pkg from 'keytar';
 import {
 	DEFAULT_PERMIT_KEYSTORE_ACCOUNT,
 	KEYSTORE_PERMIT_SERVICE_NAME,
@@ -10,6 +9,8 @@ import {
 import { URL, URLSearchParams } from 'url';
 import { setTimeout } from 'timers';
 import { Buffer } from 'buffer';
+
+const { setPassword, getPassword, deletePassword } = pkg;
 
 export enum TokenType {
 	APIToken = 'APIToken',
