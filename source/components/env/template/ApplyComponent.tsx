9ecabfef5fe82cd38fc39240f30aa21d452cc38f
Abinand P <abinand0911@gmail.com>
2025-02-17 03:07:54 +0000

    feat: added a flag to pass the template file name
    Signed-off-by: Abinand P <abinand0911@gmail.com>

--- source/components/env/template/ApplyComponent.tsx
+++ source/components/env/template/ApplyComponent.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { getFiles, ApplyTemplate } from '../../../lib/env/template/utils.js';
 import SelectInput from 'ink-select-input';
 import { Text } from 'ink';
-import { loadAuthToken } from '../../../lib/auth.js';
+import { useAuth } from '../../AuthProvider.js';
 
 type Props = {
 	apiKey?: string;
@@ -18,16 +18,32 @@ export default function ApplyComponent({ apiKey, local, template }: Props) {
 	const [errorMessage, setErrorMessage] = useState('');
 	const [successMessage, setSuccessMessage] = useState('');
 	const files = getFiles();
-	const key = apiKey || loadAuthToken();
+	const { authToken } = useAuth();
+	const key = apiKey || authToken;
 	const selectionValues = files.map(file => ({
 		label: file,
 		value: file,
 	}));
+	if (template) {
+		ApplyTemplate(template, key)
+			.then(message => {
+				if (message.startsWith('Error')) {
+					setErrorMessage(message);
+				} else {
+					setSuccessMessage(message);
+				}
+			})
+			.catch(error => {
+				setErrorMessage(
+					error instanceof Error ? error.message : (error as string),
+				);
+			});
+	}
 	const handleSelect = async (item: SelectItemType) => {
 		if (local) {
 			// To be implemented
 		} else {
-			const message = await ApplyTemplate(item.value, await key);
+			const message = await ApplyTemplate(item.value, key);
 			if (message.startsWith('Error')) {
 				setErrorMessage(message);
 			} else {
