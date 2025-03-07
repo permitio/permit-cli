afb0ff9f426086941877f1e37fc2bfcfeadb39a3
Gabriel Manor <g.e.manor@gmail.com>
2025-03-07 10:50:42 +0200

    Fix TF Server

--- terraform_server/server.js
+++ terraform_server/server.js
@@ -1,51 +1,59 @@
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
-const cors= require("cors");
-const { exec } = require("child_process");
-const bodyParser = require("body-parser");
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import cors from 'cors';
+import { execSync } from 'child_process';
+import bodyParser from 'body-parser';
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
-app.use(bodyParser.text({ type: "application/x-hcl" }));
-app.use(cors())
-const TF_FILE = path.join(__dirname, "temp/config.tf");
+app.use(bodyParser.text({ type: 'application/x-hcl' }));
+app.use(cors());
 
-const generateTfFile = (data, apiKey) => {
-  const tfContent = data.replace("{{API_KEY}}", `"` + apiKey + `"`);
-  const dirPath = path.dirname(TF_FILE);
-  if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath, { recursive: true });
+app.post('/apply', (req, res) => {
+	const tempDir = `temp-${Math.random().toString(36).substring(7)}`;
+	const TF_FILE = path.join(__dirname, `${tempDir}/config.tf`);
+
+	const generateTfFile = (data, apiKey) => {
+		const tfContent = data.replace('{{API_KEY}}', `"` + apiKey + `"`);
+		const dirPath = path.dirname(TF_FILE);
+		if (!fs.existsSync(dirPath)) {
+			fs.mkdirSync(dirPath, { recursive: true });
+		}
+
+		fs.writeFileSync(TF_FILE, tfContent, 'utf-8');
+	};
+
+	try {
+		console.log('API CALL IS RECIVED');
+		const apiKey = req.headers['authorization'];
+		if (!apiKey) {
+			return res.status(400).json({ error: 'API Key is required' });
+		}
+		generateTfFile(req.body, apiKey);
+      const { stdout, stderr } = execSync(
+        'terraform init && terraform apply -auto-approve',
+        { cwd: `${__dirname}/${tempDir}` }
+      );
+      if (stderr) {
+        res.status(500).json({ error: stderr });
+      }
+      res.json({ message: 'Terraform successful', output: stdout });
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	} finally {
+    // Remove the temp directory
+    fs.rmSync(`${__dirname}/${tempDir}`, { recursive: true });
+    console.log('APICALL COMPLETED'); 
   }
-
-  fs.writeFileSync(TF_FILE, tfContent,"utf-8");
-};
-
-app.post("/apply", (req, res) => {
-  try {
-    console.log("API CALL IS RECIVED")
-    const apiKey = req.headers["authorization"];
-    if (!apiKey) {
-      return res.status(400).json({ error: "API Key is required" });
-    }
-    generateTfFile(req.body, apiKey);
-    exec(
-      "terraform init && terraform apply -auto-approve",
-      { cwd: __dirname + "/temp" },
-      (error, stdout, stderr) => {
-        if (error) {
-          return res.status(500).json({ error: stderr || error.message });
-        }
-        res.json({ message: "Terraform successful", output: stdout });
-      },
-    );
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-  console.log("APICALL COMPLETED")
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
