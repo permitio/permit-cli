9f4b1ac63bd0e99bce6a2f41fa43bd3a1e4fd65f
35C4n0r <jaykumar20march@gmail.com>
2025-03-24 09:20:09 +0530

    feat: index command env info

--- source/commands/index.tsx
+++ source/commands/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Gradient from 'ink-gradient';
 import { Text } from 'ink';
+import { AuthProvider } from '../components/AuthProvider.js';
+import EnvironmentInfo from '../components/EnvironmentInfo.js';
 
 export default function Index() {
 	return (
-		<>
+		<AuthProvider skipLogin={true}>
 			<Text>
 				<Gradient colors={['#FF923F', '#944EEF']}>Permit CLI</Gradient> is a
 				developer swiss army knife for fine-grained authorization
 			</Text>
 			<Text>Run this command with --help for more information</Text>
-		</>
+			<EnvironmentInfo />
+		</AuthProvider>
 	);
 }
