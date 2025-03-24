2cb32ee5902af2e9026c3385b2a7afe7dc8886d0
35C4n0r <jaykumar20march@gmail.com>
2025-03-24 09:25:08 +0530

    fex: ci

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -3,6 +3,23 @@ import { render } from 'ink-testing-library';
 import { describe, vi, expect, it } from 'vitest';
 import Index from '../source/commands/index';
 import delay from 'delay';
+import * as keytar from 'keytar';
+
+
+vi.mock('keytar', () => {
+	const demoPermitKey = 'permit_key_'.concat('a'.repeat(97));
+
+	const keytar = {
+		setPassword: vi.fn().mockResolvedValue(() => {
+			return demoPermitKey
+		}),
+		getPassword: vi.fn().mockResolvedValue(() => {
+			return demoPermitKey
+		}),
+		deletePassword: vi.fn().mockResolvedValue(demoPermitKey),
+	};
+	return { ...keytar, default: keytar };
+});
 
 describe('index file', () => {
 	it('the index file should render', () => {
