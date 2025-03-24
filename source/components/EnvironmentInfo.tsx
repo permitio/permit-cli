9f222ac2b8ef197dd249aeaa347407c36bb611db
35C4n0r <jaykumar20march@gmail.com>
2025-03-24 18:46:37 +0530

    feat: improved information

--- source/components/EnvironmentInfo.tsx
+++ source/components/EnvironmentInfo.tsx
@@ -1,33 +1,62 @@
 import React, { useEffect, useState } from 'react';
 import Spinner from 'ink-spinner';
-import { Text, Box } from 'ink';
+import { Text } from 'ink';
 import { useAuth } from './AuthProvider.js';
+import { useEnvironmentApi } from '../hooks/useEnvironmentApi.js';
+import { useProjectAPI } from '../hooks/useProjectAPI.js';
+import { useOrganisationApi } from '../hooks/useOrganisationApi.js';
 
 export default function EnvironmentInfo() {
-	const [state, setState] = useState<'loading' | 'loggedin' | 'notloggedin'>(
+	const [state, setState] = useState<'loading' | 'notLoggedIn' | 'done'>(
 		'loading',
 	);
+	const [orgName, setOrgName] = useState<string | undefined>(undefined);
+	const [envName, setEnvName] = useState<string | undefined>(undefined);
+	const [projName, setProjName] = useState<string | undefined>(undefined);
+	const [error, setError] = useState<string | null>(null);
+
 	const { authToken, scope } = useAuth();
+	const { getEnvironment } = useEnvironmentApi();
+	const { getProject } = useProjectAPI();
+	const { getOrg } = useOrganisationApi();
 
 	useEffect(() => {
-		// const apiKeyScope = async () => {
-		// 	const { data, error } = await getApiKeyScope();
-		// 	if (error || data === undefined) {
-		// 		setError(error);
-		// 		return;
-		// 	}
-		// 	setScope({
-		// 		organization_id: data.organization_id,
-		// 		project_id: data.project_id,
-		// 		environment_id: data.environment_id,
-		// 	});
-		// };
+		const getScopeNames = async () => {
+			const { data: org, error: orgError } = await getOrg(
+				scope.organization_id,
+			);
+			const { data: proj, error: projError } = await getProject(
+				scope.project_id ?? '',
+			);
+			const { data: env, error: envError } = await getEnvironment(
+				scope.project_id ?? '',
+				scope.environment_id ?? '',
+			);
+			if (orgError || projError || envError) {
+				setError(
+					(orgError ?? '') + ' ' + (projError ?? '') + ' ' + (envError ?? ''),
+				);
+			}
+			setOrgName(org?.name ?? '');
+			setEnvName(env?.name ?? '');
+			setProjName(proj?.name ?? '');
+			setState('done');
+		};
+
 		if (authToken === '') {
-			setState('notloggedin');
+			setState('notLoggedIn');
 		} else {
-			setState('loggedin');
+			getScopeNames();
 		}
-	}, [authToken]);
+	}, [
+		authToken,
+		getEnvironment,
+		getOrg,
+		getProject,
+		scope.environment_id,
+		scope.organization_id,
+		scope.project_id,
+	]);
 	return (
 		<>
 			{state === 'loading' && (
@@ -35,36 +64,20 @@ export default function EnvironmentInfo() {
 					Loading Environment Info <Spinner type="dots" />
 				</Text>
 			)}
-			{state === 'notloggedin' && (
-				<Box
-					display={'flex'}
-					flexDirection={'column'}
-					borderStyle="round"
-					borderColor="cyan"
-					width={'30%'}
-					paddingLeft={1}
-				>
-					<Text color={'red'} dimColor={true}>
-						You are not logged in. Use command{' '}
-						<Text color={'green'}>login</Text> to login via cli.
-					</Text>
-				</Box>
+			{state === 'notLoggedIn' && (
+				<Text dimColor={true}>
+					You are not logged in. Run this command with login to login via cli.
+				</Text>
 			)}
-			{state === 'loggedin' && scope && (
-				<Box
-					display={'flex'}
-					flexDirection={'column'}
-					borderStyle="round"
-					borderColor="cyan"
-					width={'30%'}
-					paddingLeft={1}
-				>
-					<Text color={'green'}>You are logged in:</Text>
-					<Text color={'green'}>OrganizationId: {scope.organization_id}</Text>
-					<Text color={'green'}>ProjectId: {scope.project_id}</Text>
-					<Text color={'green'}>EnvironmentId: {scope.environment_id}</Text>
-				</Box>
+			{orgName && projName && envName && (
+				<>
+					<Text>You are logged in:</Text>
+					<Text>OrganizationId: {orgName}</Text>
+					<Text>ProjectId: {projName}</Text>
+					<Text>EnvironmentId: {envName}</Text>
+				</>
 			)}
+			{error && <Text>{error}</Text>}
 		</>
 	);
 }
--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -4,6 +4,7 @@ import { describe, vi, expect, it } from 'vitest';
 import Index from '../source/commands/index';
 import delay from 'delay';
 import * as keytar from 'keytar';
+import { getMockFetchResponse } from './utils';
 
 
 vi.mock('keytar', () => {
@@ -25,8 +26,12 @@ describe('index file', () => {
 	it('the index file should render', async() => {
 		const { lastFrame } = render(<Index />);
 		await delay(100);
-		expect(lastFrame()?.toString()).toMatch(
+		const finalFrame = lastFrame()?.toString();
+		expect(finalFrame).toMatch(
 			/Run this command with --help for more information/,
+		);
+		expect(finalFrame).toMatch(
+			/You are not logged in./,
 		);
 	});
 });
