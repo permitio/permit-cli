17b49faec73f653c83204daddff39c89c449d9de
Gabriel Manor <g.e.manor@gmail.com>
2025-03-26 14:24:40 +0200

    Update text formatting and add Box component

--- source/commands/index.tsx
+++ source/commands/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import Gradient from 'ink-gradient';
-import { Text } from 'ink';
+import { Text, Box, Newline } from 'ink';
 import { AuthProvider } from '../components/AuthProvider.js';
 import EnvironmentInfo from '../components/EnvironmentInfo.js';
 
 export default function Index() {
 	return (
 		<AuthProvider skipLogin={true}>
-			<Text>
-				<Gradient colors={['#FF923F', '#944EEF']}>Permit CLI</Gradient> is a
-				developer swiss army knife for fine-grained authorization
-			</Text>
-			<Text>Run this command with --help for more information</Text>
-			<EnvironmentInfo />
+			<Box borderStyle="single" margin={2} flexDirection="column">
+				<Text>
+					<Gradient colors={['#FF923F', '#944EEF']}>Permit CLI</Gradient> is a
+					developer swiss army knife for fine-grained authorization
+				</Text>
+				<Text>Run this command with --help for more information</Text>
+				<Newline count={1} />
+				<EnvironmentInfo />
+			</Box>
 		</AuthProvider>
 	);
 }
