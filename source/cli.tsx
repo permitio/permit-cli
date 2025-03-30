253c5fead0b7c1aa3561fe40db9fb4761503d2eb
mayabarak <maya@permit.io>
2025-03-30 15:24:11 +0300

    remove node

--- source/cli.tsx
+++ source/cli.tsx
@@ -1,54 +1,40 @@
 #!/usr/bin/env -S deno run --allow-all
 
-import React from 'react';
-import { render } from 'ink';
+function printHelp() {
+	console.log(`
+Permit.io CLI v0.1.2
 
-function App() {
+Usage:
+  permit [command] [options]
+
+Commands:
+  pdp check    Check PDP status
+  --help       Show this help message
+  --version    Show version
+`);
+}
+
+function main() {
 	const args = Deno.args;
 
 	if (args.includes('--version')) {
-		return <text>permit-cli v0.1.2</text>;
+		console.log('permit-cli v0.1.2');
+		return;
 	}
 
 	if (args.includes('--help')) {
-		return (
-			<text>
-				{`
-Permit.io CLI v0.1.2
-
-Usage:
-  permit [command] [options]
-
-Commands:
-  pdp check    Check PDP status
-  --help       Show this help message
-  --version    Show version
-`}
-			</text>
-		);
+		printHelp();
+		return;
 	}
 
 	if (args[0] === 'pdp' && args[1] === 'check') {
-		return <text>Checking PDP status...</text>;
+		console.log('Checking PDP status...');
+		return;
 	}
 
-	return (
-		<text>
-			{`
-Permit.io CLI v0.1.2
-
-Usage:
-  permit [command] [options]
-
-Commands:
-  pdp check    Check PDP status
-  --help       Show this help message
-  --version    Show version
-`}
-		</text>
-	);
+	printHelp();
 }
 
 if (import.meta.main) {
-	render(<App />);
+	main();
 }
