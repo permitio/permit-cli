00333158cf9c52f77715cdaa3a5deebea3826662
Abinand P <abinand0911@gmail.com>
2025-04-19 19:54:56 +0530

    feat: starting of the init command
    Signed-off-by: Abinand P <abinand0911@gmail.com>

---
+++ source/commands/init.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { AuthProvider } from '../components/AuthProvider.js';
+import { option } from 'pastel';
+import zod, { type infer as zInfer } from 'zod';
+
+export const description =
+	'A wizard to take users through all the steps, from configuring policy to enforcing it.';
+
+export const options = zod.object({
+	apiKey: zod
+		.string()
+		.optional()
+		.describe(option({ description: 'Your Permit.io API key' })),
+});
+
+type Props = {
+	options: zInfer<typeof options>;
+};
+
+export default function Init({ options }: Props) {
+	return (
+		<AuthProvider permit_key={options.apiKey} scope={'environment'}>
+			<></>
+		</AuthProvider>
+	);
+}
