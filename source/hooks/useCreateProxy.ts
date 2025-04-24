e49970a58052cf56bebca446b5185bc88d7fe10c
harshtech123 <chourasiya400t@gmail.com>
2025-04-24 18:37:26 +0530

    remove console.log

--- source/hooks/useCreateProxy.ts
+++ source/hooks/useCreateProxy.ts
@@ -54,7 +54,6 @@ export function useCreateProxy() {
 
 			setStatus('processing');
 			try {
-				console.log('Creating proxy with payload:', payload);
 				const result = await authenticatedApiClient().POST(
 					'/v2/facts/{proj_id}/{env_id}/proxy_configs',
 					{},
